00ebe5fd77482e306a878b60262c1d7b308d6f38
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-01-05 22:50:37 -0300

    05

--- JavaApp/src/If/calculo.java
+++ JavaApp/src/If/calculo.java
@@ -1,10 +1,10 @@
 package If;
 
-public class JavaEleitor {
+public class calculo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("  ola");
+
 	}
 
 }
