46d5c7e92145bf0046a542513cd6f5f2c3cdfd39
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-01-07 20:25:07 -0300

    for usando string

---
+++ JavaApp/src/If/DesafioFor.java
@@ -0,0 +1,21 @@
+package If;
+
+public class DesafioFor {
+
+	public static void main(String[] args) {
+		String valor = "#";
+		
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(valor);
+			valor+="#";
+		}
+		for (String v="#";!v.equals("######");v+="#") {
+			System.out.print("");
+			System.out.println(v);
+			
+			
+		}
+	}
+
+}
