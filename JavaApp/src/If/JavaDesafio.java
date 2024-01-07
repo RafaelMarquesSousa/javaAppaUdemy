f36f765a3f40cd5f71c1d6eb1ece78891ac4361e
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-01-07 20:11:13 -0300

    for

--- JavaApp/src/If/JavaDesafio.java
+++ JavaApp/src/If/JavaDesafio.java
@@ -1,10 +1,31 @@
 package If;
 
+import java.util.Scanner;
+
 public class JavaDesafio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
 
+		int quantidaNotas = 0;
+		double nota = 0;
+		double total = 0;
+
+		while (nota != -1) {
+			System.out.print("Informe nota> "
+					+ " (ou digite -1 para sair)Nota=");
+			nota = entrada.nextDouble();
+		
+			if (nota <= 10 && nota > 0) {
+				total += nota;
+				quantidaNotas++;
+			} else if (nota != -1) {
+				System.out.println("nota inavalida!!!");
+			}
+		}
+
+		System.out.println("Media de Notas= " + total / quantidaNotas);
+		entrada.close();
 	}
 
 }
