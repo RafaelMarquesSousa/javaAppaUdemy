ee34dbed3e61111b0c9db7d29fdee3adb0a0bca8
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-01-21 22:07:14 -0300

    21/01

---
+++ JavaApp/src/If/SwiftchsemBreak.java
@@ -0,0 +1,26 @@
+package If;
+
+public class SwiftchsemBreak {
+
+	public static void main(String[] args) {
+		String faixa = "amarela";
+
+		switch (faixa.toLowerCase()) {
+		case "preta":
+			System.out.println("Preta experiente");
+
+		case "marrom":
+			System.out.println("Marrom treiner");
+
+		case "verde":
+			System.out.println("verde junior");
+
+		case "amarela":
+			System.out.println("amarelo aprendiz");
+		default:
+			System.out.println("não sei nada");
+
+		}
+
+	}
+}
