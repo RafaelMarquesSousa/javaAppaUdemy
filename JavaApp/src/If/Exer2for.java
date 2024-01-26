7d4c006fd5fe9f833e63dac59045d29e19d63975
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-01-26 08:37:16 -0300

    26/01

---
+++ JavaApp/src/If/Exer2for.java
@@ -0,0 +1,19 @@
+package If;
+import java.util.Scanner;
+public class Exer2for {
+public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.println("Digite o ano: ");
+	int ano = scanner.nextInt();
+
+	boolean bissexto = ano % 4 == 0 && ((ano % 100) != 0  || ano % 400 == 0);
+
+	if (bissexto == true) {
+		System.out.println(ano + " é um ano bissexto");
+	} else {
+		System.out.println(ano + " não é um ano bissexto");
+	}
+	scanner.close();
+}
+}
---
+++ JavaApp/src/If/Exer4for.java
@@ -0,0 +1,26 @@
+package If;
+import java.util.Scanner;
+public class Exer4for {
+public static void main(String[] args) {
+	int contadorDeDivisores = 0;
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.println("\nDigite um numero para verificar se é primo:");
+	int numero = scanner.nextInt();
+
+	for (int i = 2; i < numero; i++) {
+		if (numero % i == 0) {
+			contadorDeDivisores++;
+		}
+	}
+
+	if (contadorDeDivisores == 0) {
+		System.out.println("\nO numero " + numero + " é primo.");
+	} else {
+		System.out.println("\nO numero " + numero + " não é primo.");
+	}
+
+	scanner.close();
+
+}
+}
