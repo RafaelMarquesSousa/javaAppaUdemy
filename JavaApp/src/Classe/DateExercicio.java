d0e0293eebe35db0f88af3b7c499762bad087dd5
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-02-07 15:20:44 -0300

    k

--- JavaApp/src/Classe/DateExercicio.java
+++ JavaApp/src/Classe/DateExercicio.java
@@ -4,9 +4,25 @@ public class DateExercicio {
 int ano;
 int dia ;
 String  mes ;
-
+DateExercicio(){
+	dia=1;
+	mes="janeiro";
+	ano=1970;
+}
+DateExercicio(String nova){
+	dia=17;
+	mes="Abril";
+	ano=2024;
+}
+ 
+DateExercicio(int diaInicial,String mesInicial,int anoInicail ){
+	dia=diaInicial;
+	mes=mesInicial;
+	ano=anoInicail;
+	
+}
 String obterDateFormatado() {
-	return String.format("%d/,%s/,/%d \n",dia,mes,ano);
+	return String.format("%d/%s/%d \n",dia,mes,ano);
 
 		
 	}
--- JavaApp/src/Classe/dateTeste.java
+++ JavaApp/src/Classe/dateTeste.java
@@ -1,23 +1,21 @@
 package Classe;
 
 public class dateTeste {
-public static void main(String[] args) {
-	DateExercicio d= new DateExercicio();
-	d.dia=21;
-	d.mes="feveriro";
-	d.ano=1985;
-	System.out.println(d.obterDateFormatado());
-    /* System.out.printf("%d/%s/%d",d.dia,d.mes,d.ano); 
-     Obs ;melhor forma,
-	de imprimir dados USUARIO */
-    
-	var d1=new DateExercicio();
-     d1.ano=2024;
-     d1.dia=17;
-     d1.mes="abril";
-     //tipos chamar data formatadas 
-     System.out.println(d1.obterDateFormatado());
-    // System.out.printf("     %d/%s/%d",d1.dia,d1.mes,d1.ano);
-     d1.imprimeformato();//somente imprime terminal 
-}
+	public static void main(String[] args) {
+
+		DateExercicio d = new DateExercicio();
+
+		var d1 = new DateExercicio(21, "fevereiro", 2024);
+		
+		var d2 =new DateExercicio("nova");
+		
+		String dataFormtada1 = d.obterDateFormatado();
+		System.out.println(dataFormtada1);
+		System.out.println(d1.obterDateFormatado());
+		System.out.println(d2.obterDateFormatado());
+
+		d.imprimeformato();
+		d1.imprimeformato();
+		d2.imprimeformato();
+	}
 }
