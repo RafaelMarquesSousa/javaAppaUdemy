fa2b46d01fbb89ddbc46aee9547d8e1122bc9253
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-02-07 14:33:27 -0300

    1

--- JavaApp/src/Classe/dateTeste.java
+++ JavaApp/src/Classe/dateTeste.java
@@ -7,13 +7,17 @@ public static void main(String[] args) {
 	d.mes="feveriro";
 	d.ano=1985;
 	System.out.println(d.obterDateFormatado());
-    // System.out.printf("%d/%s/%d",d.dia,d.mes,d.ano);
-     var d1=new DateExercicio();
+    /* System.out.printf("%d/%s/%d",d.dia,d.mes,d.ano); 
+     Obs ;melhor forma,
+	de imprimir dados USUARIO */
+    
+	var d1=new DateExercicio();
      d1.ano=2024;
      d1.dia=17;
      d1.mes="abril";
+     //tipos chamar data formatadas 
      System.out.println(d1.obterDateFormatado());
     // System.out.printf("     %d/%s/%d",d1.dia,d1.mes,d1.ano);
-     d1.imprimeformato();
+     d1.imprimeformato();//somente imprime terminal 
 }
 }
